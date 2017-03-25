71aa6d5da84b883e3962f6297504695cb7b63165
hcylus <hercylus@gmail.com>
2017-03-25 20:35:41 +0800

    training

--- argparsex.py
+++ argparsex.py
@@ -19,7 +19,13 @@ parser.add_argument('-u', '--user', dest='user', metavar='root', action='store',
 parser.add_argument('-p', '--pass', dest='password', metavar='xxx', action='store', type=str, help="user password")
 parser.add_argument('-P', '--port', dest='port', metavar='3306', type=int, default=3306, help="program port")
 parser.add_argument('-d', '--databases', nargs='+', help="database name")
+subparser = parser.add_subparsers(title='subcommands', description='valid commangd', help='additional help')
+subparser.add_parser('table')
+subparser.add_parser('collection')
 parser.add_argument('test', nargs='?', help="test args")
+group = parser.add_argument_group('administrator group')
+group.add_argument('--super')
+group.add_argument('power')
 args = parser.parse_args()
 # return args
 
